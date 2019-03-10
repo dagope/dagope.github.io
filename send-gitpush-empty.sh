c7a0447a542e9eac84d4e5b37f4a16235a4da704
David Gonzalo Pena <david.gonzalo@tokiota.com>
2019-03-10 03:50:06 +0100

    travis script sh

---
+++ send-gitpush-empty.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+set -eux
+
+TRAVIS_BLOG_TOKEN=${GITHUB_TOKEN:-''}
+
+if [ -z $GITHUB_TOKEN ]
+then
+    echo "Token empty"
+    exit 0
+else
+  DEPLOY_REPO="https://${GITHUB_TOKEN}@github.com/dagope/dagope.github.io.git"
+fi
+
+if [ -z "${TRAVIS_PULL_REQUEST:-''}" ]; then
+    echo "except don't publish site for pull requests"
+    exit 0
+fi  
+
+if [ "${TRAVIS_BRANCH:-''}" != "development" ]; then
+    echo "except we should only publish the development branch. stopping here"
+    exit 0
+fi
+
+echo "deploying changes: ${TRAVIS_BUILD_NUMBER:-'unknown'}"
+
+git clone --progress --depth 1 $DEPLOY_REPO _site
+
+cd _site
+
+git config --global user.name "Travis CI"
+git config --global user.email dagope+travis@gmail.com
+
+git checkout development
+git commit -m "rebuild pages: ${TRAVIS_BUILD_NUMBER:-'unknown'} " --allow-empty
+git push $DEPLOY_REPO development:development
+exit $?
